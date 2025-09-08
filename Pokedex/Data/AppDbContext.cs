b6e9f1ebc2987ec0726c0a5c3d9a8e2a80116014
GabrielMeneghelli <meneghelli.w7zin@gmail.co>
2025-09-08 20:39:30 -0300

    Criado o projeto e a camada de dados

---
+++ Pokedex/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Pokedex.Models;
+
+namespace Pokedex.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Genero> Generos { get; set; }
+        public DbSet<Pokemon> Pokemons { get; set; }
+        public DbSet<PokemonTipo> pokemonTipos { get; set; }
+        public DbSet<Regiao> Regiaos { get; set; }
+        public DbSet<Tipo> Tipos { get; set; }
+    }
+}
